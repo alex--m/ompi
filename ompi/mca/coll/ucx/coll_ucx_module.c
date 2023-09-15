c0971a2056cfbbc09403152701a64a6e2ffe0233
Alex Margolin <alex.margolin@mail.huji.ac.il>
2023-09-15 12:33:27 +0300

    coll/ucx: Neighborhood collectives support
    Signed-off-by: Alex Margolin <alex.margolin@mail.huji.ac.il>

--- ompi/mca/coll/ucx/coll_ucx_module.c
+++ ompi/mca/coll/ucx/coll_ucx_module.c
@@ -32,11 +32,13 @@ static int mca_coll_ucg_create(mca_coll_ucx_module_t *module,
                                     UCG_GROUP_PARAM_FIELD_MEMBER_INDEX |
                                     UCG_GROUP_PARAM_FIELD_CB_CONTEXT   |
                                     UCG_GROUP_PARAM_FIELD_DISTANCES    |
-                                    UCG_GROUP_PARAM_FIELD_NAME;
+                                    UCG_GROUP_PARAM_FIELD_NAME         |
+                                    UCG_GROUP_PARAM_FIELD_FLAGS;
     args.member_count             = ompi_comm_size(comm);
     args.member_index             = ompi_comm_rank(comm);
     args.cb_context               = comm;
     args.name                     = comm->c_name;
+    args.flags                    = 0;
     args.distance_type            = UCG_GROUP_DISTANCE_TYPE_ARRAY;
     args.distance_array           = alloca(args.member_count *
                                            sizeof(*args.distance_array));
@@ -79,7 +81,12 @@ static int mca_coll_ucg_create(mca_coll_ucx_module_t *module,
         }
     }
 
-    /* TODO: use additional info, such as OMPI_COMM_IS_CART(comm) */
+    if (OMPI_COMM_IS_CART(comm)  ||
+        OMPI_COMM_IS_GRAPH(comm) ||
+        OMPI_COMM_IS_DIST_GRAPH(comm)) {
+        args.flags |= UCG_GROUP_CREATE_FLAG_NEIGHBORHOOD;
+    }
+
     /* TODO: add support for comm->c_remote_comm  */
     /* TODO: add support for sparse group storage */
 
@@ -127,23 +134,109 @@ static void mca_coll_ucx_module_construct(mca_coll_ucx_module_t *module)
     memset(&module->super.super + 1, 0,
            sizeof(*module) - sizeof(module->super.super));
 
-    module->super.coll_module_enable  = mca_coll_ucx_module_enable;
-    module->super.coll_allreduce      = mca_coll_ucx_component.stable_reduce ?
-                                        mca_coll_ucx_allreduce_stable :
-                                        mca_coll_ucx_allreduce;
-/*    module->super.coll_iallreduce     = mca_coll_ucx_iallreduce; */
-/*    module->super.coll_allreduce_init = mca_coll_ucx_allreduce_init; */
-    module->super.coll_barrier        = mca_coll_ucx_barrier;
-    module->super.coll_bcast          = mca_coll_ucx_bcast;
-    module->super.coll_reduce         = mca_coll_ucx_component.stable_reduce ?
-                                        mca_coll_ucx_reduce_stable :
-                                        mca_coll_ucx_reduce;
-    module->super.coll_scatter        = mca_coll_ucx_scatter;
-    module->super.coll_scatterv       = mca_coll_ucx_scatterv;
-    module->super.coll_gather         = mca_coll_ucx_gather;
-    module->super.coll_gatherv        = mca_coll_ucx_gatherv;
-/*    module->super.coll_allgather      = mca_coll_ucx_allgather; */
-    module->super.coll_alltoall       = mca_coll_ucx_alltoall;
+    module->super.coll_module_enable             = mca_coll_ucx_module_enable;
+
+    /* blocking functions */
+    module->super.coll_allgather                 = mca_coll_ucx_allgather;
+    module->super.coll_allgatherv                = mca_coll_ucx_allgatherv;
+    module->super.coll_allreduce                 = mca_coll_ucx_component.stable_reduce ?
+                                                   mca_coll_ucx_allreduce_stable :
+                                                   mca_coll_ucx_allreduce;
+    module->super.coll_alltoall                  = mca_coll_ucx_alltoall;
+    module->super.coll_alltoallv                 = mca_coll_ucx_alltoallv;
+    module->super.coll_alltoallw                 = mca_coll_ucx_alltoallw;
+    module->super.coll_barrier                   = mca_coll_ucx_barrier;
+    module->super.coll_bcast                     = mca_coll_ucx_bcast;
+    module->super.coll_exscan                    = mca_coll_ucx_exscan;
+    module->super.coll_gather                    = mca_coll_ucx_gather;
+    module->super.coll_gatherv                   = mca_coll_ucx_gatherv;
+    module->super.coll_reduce                    = mca_coll_ucx_component.stable_reduce ?
+                                                   mca_coll_ucx_reduce_stable :
+                                                   mca_coll_ucx_reduce;
+    module->super.coll_reduce_scatter            = mca_coll_ucx_component.stable_reduce ?
+                                                   mca_coll_ucx_reduce_scatter_stable :
+                                                   mca_coll_ucx_reduce_scatter;
+    module->super.coll_reduce_scatter_block      = mca_coll_ucx_component.stable_reduce ?
+                                                   mca_coll_ucx_reduce_scatter_block_stable :
+                                                   mca_coll_ucx_reduce_scatter_block;
+    module->super.coll_scan                      = mca_coll_ucx_scan;
+    module->super.coll_scatter                   = mca_coll_ucx_scatter;
+    module->super.coll_scatterv                  = mca_coll_ucx_scatterv;
+
+    /* nonblocking functions */
+    module->super.coll_iallgather                = mca_coll_ucx_iallgather;
+    module->super.coll_iallgatherv               = mca_coll_ucx_iallgatherv;
+    module->super.coll_iallreduce                = mca_coll_ucx_component.stable_reduce ?
+                                                   mca_coll_ucx_iallreduce_stable :
+                                                   mca_coll_ucx_iallreduce;
+    module->super.coll_ialltoall                 = mca_coll_ucx_ialltoall;
+    module->super.coll_ialltoallv                = mca_coll_ucx_ialltoallv;
+    module->super.coll_ialltoallw                = mca_coll_ucx_ialltoallw;
+    module->super.coll_ibarrier                  = mca_coll_ucx_ibarrier;
+    module->super.coll_ibcast                    = mca_coll_ucx_ibcast;
+    module->super.coll_iexscan                   = mca_coll_ucx_iexscan;
+    module->super.coll_igather                   = mca_coll_ucx_igather;
+    module->super.coll_igatherv                  = mca_coll_ucx_igatherv;
+    module->super.coll_ireduce                   = mca_coll_ucx_component.stable_reduce ?
+                                                   mca_coll_ucx_ireduce_stable :
+                                                   mca_coll_ucx_ireduce;
+    module->super.coll_ireduce_scatter           = mca_coll_ucx_component.stable_reduce ?
+                                                   mca_coll_ucx_ireduce_scatter_stable :
+                                                   mca_coll_ucx_ireduce_scatter;
+    module->super.coll_ireduce_scatter_block     = mca_coll_ucx_component.stable_reduce ?
+                                                   mca_coll_ucx_ireduce_scatter_block_stable :
+                                                   mca_coll_ucx_ireduce_scatter_block;
+    module->super.coll_iscan                     = mca_coll_ucx_iscan;
+    module->super.coll_iscatter                  = mca_coll_ucx_iscatter;
+    module->super.coll_iscatterv                 = mca_coll_ucx_iscatterv;
+
+    /* persistent functions */
+    module->super.coll_allgather_init            = mca_coll_ucx_allgather_init;
+    module->super.coll_allgatherv_init           = mca_coll_ucx_allgatherv_init;
+    module->super.coll_allreduce_init            = mca_coll_ucx_component.stable_reduce ?
+                                                   mca_coll_ucx_allreduce_stable_init :
+                                                   mca_coll_ucx_allreduce_init;
+    module->super.coll_alltoall_init             = mca_coll_ucx_alltoall_init;
+    module->super.coll_alltoallv_init            = mca_coll_ucx_alltoallv_init;
+    module->super.coll_alltoallw_init            = mca_coll_ucx_alltoallw_init;
+    module->super.coll_barrier_init              = mca_coll_ucx_barrier_init;
+    module->super.coll_bcast_init                = mca_coll_ucx_bcast_init;
+    module->super.coll_exscan_init               = mca_coll_ucx_exscan_init;
+    module->super.coll_gather_init               = mca_coll_ucx_gather_init;
+    module->super.coll_gatherv_init              = mca_coll_ucx_gatherv_init;
+    module->super.coll_reduce_init               = mca_coll_ucx_component.stable_reduce ?
+                                                   mca_coll_ucx_reduce_stable_init :
+                                                   mca_coll_ucx_reduce_init;
+    module->super.coll_reduce_scatter_init       = mca_coll_ucx_component.stable_reduce ?
+                                                   mca_coll_ucx_reduce_scatter_stable_init :
+                                                   mca_coll_ucx_reduce_scatter_init;
+    module->super.coll_reduce_scatter_block_init = mca_coll_ucx_component.stable_reduce ?
+                                                   mca_coll_ucx_reduce_scatter_block_stable_init :
+                                                   mca_coll_ucx_reduce_scatter_block_init;
+    module->super.coll_scan_init                 = mca_coll_ucx_scan_init;
+    module->super.coll_scatter_init              = mca_coll_ucx_scatter_init;
+    module->super.coll_scatterv_init             = mca_coll_ucx_scatterv_init;
+
+    /* neighborhood functions */
+    module->super.coll_neighbor_allgather        = mca_coll_ucx_neighbor_allgather;
+    module->super.coll_neighbor_allgatherv       = mca_coll_ucx_neighbor_allgatherv;
+    module->super.coll_neighbor_alltoall         = mca_coll_ucx_neighbor_alltoall;
+    module->super.coll_neighbor_alltoallv        = mca_coll_ucx_neighbor_alltoallv;
+    module->super.coll_neighbor_alltoallw        = mca_coll_ucx_neighbor_alltoallw;
+
+    /* nonblocking neighborhood functions */
+    module->super.coll_ineighbor_allgather       = mca_coll_ucx_ineighbor_allgather;
+    module->super.coll_ineighbor_allgatherv      = mca_coll_ucx_ineighbor_allgatherv;
+    module->super.coll_ineighbor_alltoall        = mca_coll_ucx_ineighbor_alltoall;
+    module->super.coll_ineighbor_alltoallv       = mca_coll_ucx_ineighbor_alltoallv;
+    module->super.coll_ineighbor_alltoallw       = mca_coll_ucx_ineighbor_alltoallw;
+
+    /* persistent neighborhood functions */
+    module->super.coll_neighbor_allgather_init   = mca_coll_ucx_neighbor_allgather_init;
+    module->super.coll_neighbor_allgatherv_init  = mca_coll_ucx_neighbor_allgatherv_init;
+    module->super.coll_neighbor_alltoall_init    = mca_coll_ucx_neighbor_alltoall_init;
+    module->super.coll_neighbor_alltoallv_init   = mca_coll_ucx_neighbor_alltoallv_init;
+    module->super.coll_neighbor_alltoallw_init   = mca_coll_ucx_neighbor_alltoallw_init;
 }
 
 static void mca_coll_ucx_module_destruct(mca_coll_ucx_module_t *module) {
