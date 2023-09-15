71c2c0c58bde3760df4bfe0acbe8ead785dafd2e
Alex Margolin <alex.margolin@mail.huji.ac.il>
2023-09-15 12:42:04 +0300

    coll/ucx: Added support for time imbalance reports
    Signed-off-by: Alex Margolin <alex.margolin@mail.huji.ac.il>

---
+++ ompi/mpi/c/comm_get_last_imbalance.c
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2004-2007 The Trustees of Indiana University and Indiana
+ *                         University Research and Technology
+ *                         Corporation.  All rights reserved.
+ * Copyright (c) 2004-2005 The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ * Copyright (c) 2004-2008 High Performance Computing Center Stuttgart,
+ *                         University of Stuttgart.  All rights reserved.
+ * Copyright (c) 2004-2005 The Regents of the University of California.
+ *                         All rights reserved.
+ * Copyright (c) 2006-2018 Cisco Systems, Inc.  All rights reserved
+ * Copyright (c) 2015-2017 Research Organization for Information Science
+ *                         and Technology (RIST). All rights reserved.
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
+ *
+ * $HEADER$
+ */
+
+#include "ompi_config.h"
+
+#include <string.h>
+
+#include "opal/mca/threads/mutex.h"
+#include "opal/util/string_copy.h"
+
+#include "ompi/mpi/c/bindings.h"
+#include "ompi/runtime/params.h"
+#include "ompi/communicator/communicator.h"
+#include "ompi/errhandler/errhandler.h"
+#include "ompi/totalview.h"
+#include "ompi/memchecker.h"
+
+#if OMPI_BUILD_MPI_PROFILING
+#if OPAL_HAVE_WEAK_SYMBOLS
+#pragma weak MPI_Comm_get_last_imbalance = PMPI_Comm_get_last_imbalance
+#endif
+#define MPI_Comm_get_last_imbalance PMPI_Comm_get_last_imbalance
+#endif
+
+static const char FUNC_NAME[] = "MPI_Comm_get_last_imbalance";
+
+
+double MPI_Comm_get_last_imbalance(MPI_Comm comm)
+{
+    double ret;
+
+    MEMCHECKER(
+        memchecker_comm(comm);
+    );
+
+    if ( MPI_PARAM_CHECK ) {
+        OMPI_ERR_INIT_FINALIZE(FUNC_NAME);
+        if (ompi_comm_invalid(comm)) {
+            return OMPI_ERRHANDLER_NOHANDLE_INVOKE(MPI_ERR_COMM, FUNC_NAME);
+        }
+    }
+
+     return comm->c_imbalance;
+}
